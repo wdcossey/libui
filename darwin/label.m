13e6d7c98a4c1886de1f3b4a99ce33fcad500807
Rustam Gamidov <rustam.gamidov@beetroot.com.ua>
2020-04-20 10:24:37 +0300

    label: implement font setter and text alignment functions on macos

--- darwin/label.m
+++ darwin/label.m
@@ -18,6 +18,16 @@ void uiLabelSetText(uiLabel *l, const char *text)
 	[l->textfield setStringValue:uiprivToNSString(text)];
 }
 
+void uiLabelSetFont(uiLabel *l, const char *name, int size, int weight, int italic)
+{
+	NSFontTraitMask traits = 0;
+	if (italic > 0)
+		traits = NSFontItalicTrait;
+	NSFont* font = [[NSFontManager sharedFontManager]
+		fontWithFamily:uiprivToNSString(name) traits:traits weight:systemTextWeigth((uiTextWeight)weight) size:size];
+	[l->textfield setFont:font];
+}
+
 NSTextField *uiprivNewLabel(NSString *str)
 {
 	NSTextField *tf;
@@ -38,6 +48,19 @@ uiLabel *uiNewLabel(const char *text)
 	uiDarwinNewControl(uiLabel, l);
 
 	l->textfield = uiprivNewLabel(uiprivToNSString(text));
+
+	return l;
+}
+
+
+uiLabel *uiNewCenteredLabel(const char *text)
+{
+	uiLabel *l;
+
+	uiDarwinNewControl(uiLabel, l);
+
+	l->textfield = uiprivNewLabel(uiprivToNSString(text));
+	[l->textfield setAlignment:NSCenterTextAlignment];
 
 	return l;
 }
